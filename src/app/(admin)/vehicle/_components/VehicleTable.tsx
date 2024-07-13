e0f306b00f338b8805bbe3713342aacd0f8a4932
Jahidul Islam <jahidul.islam.sec@gmail.com>
2024-07-13 15:57:15 +0600

    feat: adding action buttons and tooltips

--- src/app/(admin)/vehicle/_components/VehicleTable.tsx
+++ src/app/(admin)/vehicle/_components/VehicleTable.tsx
@@ -1,6 +1,17 @@
-import { Table, TableBody, TableCell, TableHead, TableHeader, TableRow } from "@/components/ui/table";
+import Tooltips from "@/components/Tooltips";
+import { Button } from "@/components/ui/button";
+import {
+  Table,
+  TableBody,
+  TableCell,
+  TableHead,
+  TableHeader,
+  TableRow,
+} from "@/components/ui/table";
+
 import { vehicles } from "@/lib/data";
 import { formatDate } from "@/lib/formatter";
+import { Edit, ShoppingCart, Trash } from "lucide-react";
 import React from "react";
 
 const VehicleTable = ({ vehicle }: { vehicle: typeof vehicles }) => {
@@ -18,17 +29,44 @@ const VehicleTable = ({ vehicle }: { vehicle: typeof vehicles }) => {
         </TableHeader>
 
         <TableBody>
-            {
-                vehicle.map(item => (
-                    <TableRow key={item.id}>
-                        <TableCell>{item.id}</TableCell>
-                        <TableCell>{item.model}</TableCell>
-                        <TableCell>{item.chesis_number}</TableCell>
-                        <TableCell>{formatDate(item.date)}</TableCell>
-                        <TableCell></TableCell>
-                    </TableRow>
-                ))
-            }
+          {vehicle.map((item) => (
+            <TableRow key={item.id}>
+              <TableCell>{item.id}</TableCell>
+              <TableCell>{item.model}</TableCell>
+              <TableCell>{item.chesis_number}</TableCell>
+              <TableCell>{formatDate(item.date)}</TableCell>
+              <TableCell className="flex gap-1">
+                <Tooltips title="Sell Vehicle">
+                  <Button
+                    size={"icon"}
+                    variant={"outline"}
+                    className="rounded-full"
+                  >
+                    <ShoppingCart className="size-4" />
+                  </Button>
+                </Tooltips>
+
+                <Tooltips title="Edit">
+                  <Button
+                    size={"icon"}
+                    variant={"outline"}
+                    className="rounded-full"
+                  >
+                    <Edit className="size-4" />
+                  </Button>
+                </Tooltips>
+                <Tooltips title="Delete">
+                  <Button
+                    size={"icon"}
+                    variant={"destructive"}
+                    className="rounded-full"
+                  >
+                    <Trash className="size-4" />
+                  </Button>
+                </Tooltips>
+              </TableCell>
+            </TableRow>
+          ))}
         </TableBody>
       </Table>
     </>
