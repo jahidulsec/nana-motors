df66f4066c371d33159abd75d8e5cc2e24909085
Jahidul Islam <jahidul.islam.sec@gmail.com>
2024-08-03 23:15:16 +0600

    feat: vehicle page stats from db

--- src/app/(admin)/vehicle/page.tsx
+++ src/app/(admin)/vehicle/page.tsx
@@ -1,14 +1,18 @@
 import React, { Suspense } from "react";
 import Section from "@/components/Section";
 import VehicleTable from "./_components/VehicleTable";
-import { wait } from "@/lib/wait";
-import { vehicles } from "@/lib/data";
 import FilterSection from "./_components/FilterSection";
 import TableSkeleton from "@/components/TableSkeleton";
 import db from "../../../../db/db";
 import { Vehicle } from "@prisma/client";
 
-const VehiclePage = () => {
+const VehiclePage = async() => {
+
+  const [availableCount, inProgressCount] = await Promise.all([
+    db.vehicle.count({where: {status: "available"}}),
+    db.vehicle.count({where: {status: "in-emi"}})
+  ])
+
   return (
     <Section>
       {/* header */}
@@ -18,14 +22,14 @@ const VehiclePage = () => {
         {/* cards */}
         <div className="cards flex gap-5 justify-center items-center ">
           <article className="flex flex-col gap-1 justify-center items-center">
-            <h4 className="text-3xl font-semibold text-primary">94</h4>
+            <h4 className="text-3xl font-semibold text-primary">{availableCount}</h4>
             <p className="text-gray-400 text-[12px]">In Stock</p>
           </article>
 
           <div className="bg-gray-400 h-[2.5rem] w-[1px]"></div>
 
           <article className="flex flex-col gap-1 justify-center items-center">
-            <h4 className="text-3xl font-semibold text-primary">20</h4>
+            <h4 className="text-3xl font-semibold text-primary">{inProgressCount}</h4>
             <p className="text-gray-400 text-[12px]">In progress</p>
           </article>
         </div>
