ca0a57eb8cc061cd8f981645b85be93db0394ced
Jahidul Islam <jahidul.islam.sec@gmail.com>
2024-08-09 15:14:32 +0600

    feat: prepopulate vehicle data in sell form

--- src/app/(admin)/vehicle/sell/[id]/_components/SellVehicleForm.tsx
+++ src/app/(admin)/vehicle/sell/[id]/_components/SellVehicleForm.tsx
@@ -8,19 +8,18 @@ import { Label } from "@/components/ui/label";
 import Section from "@/components/Section";
 import { useDebounce } from "@/hooks/useDebounce";
 import db from "../../../../../../../db/db";
-import { Customer } from "@prisma/client";
+import { Customer, Vehicle } from "@prisma/client";
 
-const SellVehicleForm = () => {
+const SellVehicleForm = ({ vehicle }: { vehicle: Vehicle }) => {
   const [nid, setNid] = useState<string>("");
   const [customerData, setCustomerData] = useState<Customer>();
   const debonceValue = useDebounce(nid);
 
-
   const handleCustomer = async () => {
     // const data = await db.customer.findUnique({ where: { nid: nid } });
     const res = await fetch("/api/sell/" + nid);
-    const data = await res.json()
-    setCustomerData(data)
+    const data = await res.json();
+    setCustomerData(data);
   };
 
   useEffect(() => {
@@ -57,23 +56,44 @@ const SellVehicleForm = () => {
           <div className="grid grid-cols-1 md:grid-cols-3 gap-3 my-5 border-b pb-5">
             <p>
               <Label htmlFor="name">Name</Label>
-              <Input id="name" defaultValue={customerData && customerData?.name} placeholder="Enter customer full name" />
+              <Input
+                id="name"
+                defaultValue={customerData && customerData?.name}
+                placeholder="Enter customer full name"
+              />
             </p>
             <p>
               <Label htmlFor="fatherName">Father&apos;s Name</Label>
-              <Input id="fatherName" defaultValue={customerData && customerData.fatherName || '' } placeholder="Enter father name" />
+              <Input
+                id="fatherName"
+                defaultValue={(customerData && customerData.fatherName) || ""}
+                placeholder="Enter father name"
+              />
             </p>
             <p>
               <Label htmlFor="motherName">Mother&apos;s Name</Label>
-              <Input id="motherName" defaultValue={customerData && customerData.motherName || '' } placeholder="Enter mother name" />
+              <Input
+                id="motherName"
+                defaultValue={(customerData && customerData.motherName) || ""}
+                placeholder="Enter mother name"
+              />
             </p>
             <p>
               <Label htmlFor="spouseName">Spouse Name</Label>
-              <Input id="spouseName" defaultValue={customerData && customerData.spouseName || '' } placeholder="Enter spouse name" />
+              <Input
+                id="spouseName"
+                defaultValue={(customerData && customerData.spouseName) || ""}
+                placeholder="Enter spouse name"
+              />
             </p>
             <p>
               <Label htmlFor="mobile">Mobile</Label>
-              <Input type="text" id="mobile" placeholder="ex. 01777 333444" defaultValue={customerData && customerData.mobile || '' } />
+              <Input
+                type="text"
+                id="mobile"
+                placeholder="ex. 01777 333444"
+                defaultValue={(customerData && customerData.mobile) || ""}
+              />
             </p>
             <p>
               <Label htmlFor="mobile_2">Mobile 2</Label>
@@ -96,23 +116,43 @@ const SellVehicleForm = () => {
           <div className="address grid grid-cols-1 md:grid-cols-3 gap-3">
             <p>
               <Label htmlFor="houseName">House Name</Label>
-              <Input id="houseName" defaultValue={customerData && customerData.houseName || '' } placeholder="Enter house name or number" />
+              <Input
+                id="houseName"
+                defaultValue={(customerData && customerData.houseName) || ""}
+                placeholder="Enter house name or number"
+              />
             </p>
             <p>
               <Label htmlFor="village">Village</Label>
-              <Input id="village" defaultValue={customerData && customerData.village || '' } placeholder="Enter village name" />
+              <Input
+                id="village"
+                defaultValue={(customerData && customerData.village) || ""}
+                placeholder="Enter village name"
+              />
             </p>
             <p>
               <Label htmlFor="postOffice">Post Office</Label>
-              <Input id="postOffice" defaultValue={customerData && customerData.postOffice || '' } placeholder="Enter post office name" />
+              <Input
+                id="postOffice"
+                defaultValue={(customerData && customerData.postOffice) || ""}
+                placeholder="Enter post office name"
+              />
             </p>
             <p>
               <Label htmlFor="upazilla">Upazilla</Label>
-              <Input id="upazilla" defaultValue={customerData && customerData.upazilla || '' } placeholder="Enter upazilla name" />
+              <Input
+                id="upazilla"
+                defaultValue={(customerData && customerData.upazilla) || ""}
+                placeholder="Enter upazilla name"
+              />
             </p>
             <p>
               <Label htmlFor="district">District</Label>
-              <Input id="district" defaultValue={customerData && customerData.district || '' } placeholder="Enter district" />
+              <Input
+                id="district"
+                defaultValue={(customerData && customerData.district) || ""}
+                placeholder="Enter district"
+              />
             </p>
           </div>
         </Section>
@@ -124,15 +164,21 @@ const SellVehicleForm = () => {
           </h3>
           <div className="my-5 grid grid-cols-3 gap-3">
             <p>
-              <Label htmlFor="vehicle_no">Vehicle No.</Label>
-              <Input id="vehicle_no" placeholder="Enter vehicle number" />
+              <Label htmlFor="engineNo">Vehicle No.</Label>
+              <Input
+                id="engineNo"
+                defaultValue={vehicle && vehicle.engineNo}
+                placeholder="Enter vehicle number"
+              />
             </p>
             <p>
               <Label htmlFor="price">Price</Label>
               <Input
                 type="number"
                 id="price"
-                placeholder="Enter selling price"
+                placeholder={`Purchase price ${
+                  vehicle && vehicle.purchasePrice
+                }`}
               />
             </p>
             <p className="flex flex-col gap-1">
--- src/app/(admin)/vehicle/sell/[id]/page.tsx
+++ src/app/(admin)/vehicle/sell/[id]/page.tsx
@@ -1,11 +1,16 @@
 import React from "react";
 import SellVehicleForm from "./_components/SellVehicleForm";
+import db from "../../../../../../db/db";
+import { Vehicle } from "@prisma/client";
 
-const VehicleSellPage = () => {
+const VehicleSellPage = async({params}: {params: {id: string}}) => {
+  
+  const vehicleData = await db.vehicle.findUnique({where: {id: Number(params.id)}})
+
   return (
     <>
       {/* form */}
-      <SellVehicleForm />
+      <SellVehicleForm vehicle={vehicleData as Vehicle} />
     </>
   );
 };
